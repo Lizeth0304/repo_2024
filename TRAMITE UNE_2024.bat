f81601e8b93c86d0344af4156d115144a0359333
Ingrid Lizeth Crisostomo Espiritu <icrisostomo@une.edu.pe>
2024-01-02 10:31:08 -0500

    cambios 2024

---
+++ TRAMITE UNE_2024.bat
@@ -0,0 +1,26 @@
+@echo off
+
+title Ejecutar Tramite Rectorado
+
+cd D:\6_11_PROYECTO_RECTORADO_CON_BD\ServidorBD_2024
+start /B node index.js
+
+cd D:\6_11_PROYECTO_RECTORADO_CON_BD\Proyecto_dash_2024
+echo Ejecutando el proyecto frontend...
+start /B npm run dev 
+
+cd D:\6_11_PROYECTO_RECTORADO_CON_BD\Backend_Tramite
+echo Ejecutando el proyecto backend...
+start /B npm run dev 
+
+start http://localhost:5173/
+echo Proyecto Iniciado Correctamente.
+
+cd D:\6_11_PROYECTO_RECTORADO_CON_BD\Conversion_pdf
+echo Ejecutando el proyecto python...
+@echo off
+set FLASK_APP=app.py
+set FLASK_ENV=production
+waitress-serve --port=5000 app:app
+
+
